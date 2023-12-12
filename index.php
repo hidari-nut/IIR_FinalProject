f2fdb2fea115a518c203c4d4203fccf3d57ae331
hidari-nut <madevis02@gmail.com>
2023-12-12 20:22:42 +0700

    new update

--- index.php
+++ index.php
@@ -12,14 +12,60 @@
 </head>
 
 <body>
-    <div class="page-type">Home|Crawling</div>
-    <div class="page-title">Data Crawling from Google Scholar</div>
-    <div class="search-section">
-        <span class="text">Keyword:</span>
-        <input type="text" class="search-input" placeholder="Search here">
-        <button class="search-button">Crawl</button>
-    </div>
-    <script src="" async defer></script>
+    <?php
+    $sampleData = [
+        [
+            'id' => 1,
+            'title' => 'Sample Title 1',
+            'number_citations' => 10,
+            'authors' => 'Author A, Author B',
+            'abstract' => 'This is the abstract for the first sample entry.',
+            'similarity' => 0.85
+        ],
+        [
+            'id' => 2,
+            'title' => 'Sample Title 2',
+            'number_citations' => 15,
+            'authors' => 'Author C, Author D',
+            'abstract' => 'This is the abstract for the second sample entry.',
+            'similarity' => 0.92
+        ],
+        [
+            'id' => 3,
+            'title' => 'Sample Title 3',
+            'number_citations' => 8,
+            'authors' => 'Author E, Author F',
+            'abstract' => 'This is the abstract for the third sample entry.',
+            'similarity' => 0.78
+        ]
+    ];
+    ?>
+    <form method="POST" action="">
+        <div class="page-type">Home|Crawling</div>
+        <div class="page-title">Data Crawling from Google Scholar</div>
+        <div class="search-section">
+            <span class="text">Keyword:</span>
+            <input type="text" class="search-input" name="keyword" placeholder="Search here">
+            <button type="submit" name="crawl_button" class="search-button">Crawl</button>
+        </div>
+        <script src="" async defer></script>
+        <?php
+        if (isset($_POST['crawl_button'])) {
+            foreach ($sampleData as $data) {
+                echo "<div class='result' style='font-family: Roboto, sans-serif;'>";
+                echo "<p><strong>Title:</strong> <br>" . $data['title'] . "</p>";
+                echo "<p><strong>Authors:</strong> <br>" . $data['authors'] . "</p>";
+                echo "<p><strong>Abstract:</strong> <br>" . $data['abstract'] . "</p>";
+                echo "-----------------------------";
+                echo "<p><strong>Number of Citations:</strong> " . $data['number_citations'] . "</p>";
+                if ($index !== count($sampleData) - 1) {
+                    echo "<div class='separator'></div>";
+                }
+                echo "</div>";
+            }
+        }
+        ?>
+    </form>
 </body>
 
 </html>
