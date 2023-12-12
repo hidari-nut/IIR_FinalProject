3d2bed25a3c3433a28ec8ee4798d048f4155cd58
hidari-nut <madevis02@gmail.com>
2023-12-12 19:14:44 +0700

    base

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>IIR Final Project</title>
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="https://fonts.googleapis.com/css?family=Roboto:400,500,700&display=swap" rel="stylesheet">
+    <link href="style.css" rel="stylesheet">
+</head>
+
+<body>
+    <div class="page-type">Home|Crawling</div>
+    <div class="page-title">Data Crawling from Google Scholar</div>
+    <div class="search-section">
+        <span class="text">Keyword:</span>
+        <input type="text" class="search-input" placeholder="Search here">
+        <button class="search-button">Crawl</button>
+    </div>
+    <script src="" async defer></script>
+</body>
+
+</html>
